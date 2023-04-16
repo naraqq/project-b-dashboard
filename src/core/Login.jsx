fab4f355e9ec4119addaf011c0d27aa9bd1753af
narmandakh.g <narmandakh.g@ddishtv.mn>
2023-04-16 14:06:11 +0800

    dupate

--- src/core/Login.jsx
+++ src/core/Login.jsx
@@ -1,5 +1,8 @@
 import { useNavigate } from "react-router-dom";
 import $ from "jquery";
+import axios from "axios";
+import { useState } from "react";
+import { ToastContainer, toast } from "react-toastify";
 const mobileScreen = window.matchMedia("(max-width: 990px )");
 $(document).ready(function () {
   $(".dashboard-nav-dropdown-toggle").click(function () {
@@ -18,29 +21,59 @@ $(document).ready(function () {
     }
   });
 });
+
 function Login() {
   const navigate = useNavigate();
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const data = {
+    userName: username,
+    password: password,
+  };
+  const handleLogin = () => {
+    axios
+      .post("http://installnaran.mn:9000/v1/Login/login", data)
+      .then((res) => {
+        console.log(res.data);
+        if (res.data.isSuccess == true) {
+          navigate("/home");
+          window.location.reload();
+        } else {
+          toast.info(res.data.resultMessage, {
+            position: "bottom-right",
+          });
+        }
+      })
+      .catch((err) => console.log(err));
+  };
   return (
     <div className="body">
+      <ToastContainer />
       <div className="login">
         <h1 className="mb-5 !text-[25px]">Install Naran XXK</h1>
         <form method="post">
           <input
+            onChange={(e) => {
+              setUsername(e.target.value);
+            }}
             type="text"
             name="u"
             placeholder="Нэвтрэх нэр"
             required="required"
           />
           <input
+            onChange={(e) => {
+              setPassword(e.target.value);
+            }}
             type="password"
             name="p"
             placeholder="Нууц үг"
             required="required"
           />
           <button
-            onClick={() => {
-              navigate("/home");
-              window.location.reload();
+            onClick={(e) => {
+              e.preventDefault();
+              handleLogin();
             }}
             className="btn btn-primary btn-block btn-large"
           >
